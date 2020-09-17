b26e2fa935b505d150f0af408503a312d9db3233
BartlomiejBak <bakbartlomiej88>
2020-09-17 13:08:35 +0200

    CRUD implementation with Spring Data REST

--- src/main/java/pl/bartekbak/imperator/dao/ContactRepository.java
+++ src/main/java/pl/bartekbak/imperator/dao/ContactRepository.java
@@ -1,7 +1,9 @@
 package pl.bartekbak.imperator.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import pl.bartekbak.imperator.entity.Contact;
 
+@RepositoryRestResource(path = "data") //optional, only to specify mapping address
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 }
